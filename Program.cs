b187a985d8c826738d45273bb36c4f0cd8f9c4a4
Matheus de Lara Calache <matheuscalache@funda.nl>
2021-10-29 16:19:18 +0200

    adding initial code for the example

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandDispatcherAndHandlerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dispatcher = new Dispatcher();
+            dispatcher.Dispatch(new SomeCommand { MyProperty = "test" });
+        }
+    }
+
+    public interface ICommandHandler<TCommand> where TCommand : class
+    {
+        void Handle(TCommand command);
+    }
+
+    public class SomeCommandHandler : ICommandHandler<SomeCommand>
+    {
+        public void Handle(SomeCommand command)
+        {
+            Console.WriteLine($"SomeCommandHandler.Value: {command.MyProperty}");
+        }
+    }
+
+    public class SomeCommandHandler2 : ICommandHandler<SomeCommand>
+    {
+        public void Handle(SomeCommand command)
+        {
+            Console.WriteLine($"SomeCommandHandler2.Value: {command.MyProperty}");
+        }
+    }
+
+    public class SomeCommand
+    {
+        public string MyProperty { get; set; }
+    }
+
+    public class Dispatcher
+    {
+        public void Dispatch<TCommand>(TCommand command) where TCommand : class
+        {
+            Type handler = typeof(ICommandHandler<>);
+            
+            Type handlerType = handler.MakeGenericType(command.GetType());
+
+            Type[] concreteTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass && t.GetInterfaces().Contains(handlerType)).ToArray();
+
+            if (!concreteTypes.Any()) return;
+
+            foreach (var type in concreteTypes)
+            {
+                var concreteHandler = Activator.CreateInstance(type) as ICommandHandler<TCommand>;
+
+                concreteHandler?.Handle(command);
+            }
+        }
+    }
+}
